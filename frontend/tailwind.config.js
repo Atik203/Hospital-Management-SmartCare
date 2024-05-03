236ca74a8092f7790ab89deb1f08bcd1ad3cccb4
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-05-03 21:11:05 +0600

    fix responsive

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,20 +1,20 @@
-import { nextui } from '@nextui-org/react';
 
+import { nextui } from '@nextui-org/react';
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './src/**/*.{js,jsx,ts,tsx}',
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
-
-],
+   content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+  ],
   theme: {
     extend: {
-      colors: {
-        navPrimary: '#007e85',
-        
-      },
+      colors:{
+        navPrimary: "#007E85",
+      }
     },
   },
-  plugins: [require("daisyui"),nextui()],
+  darkMode: "class",
+  plugins: [nextui(),require("daisyui")],
 }
 
