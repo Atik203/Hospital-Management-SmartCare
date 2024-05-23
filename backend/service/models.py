cf2ac86dbe1c50ef0c459adec828dcf70cbb224a
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-05-23 13:03:58 +0600

    created service model

--- backend/service/models.py
+++ backend/service/models.py
@@ -5,4 +5,7 @@ from django.db import models
 class Service(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField()
-    image = models.ImageField(upload_to='services/images/')
+    image = models.ImageField(upload_to='service/images/')
+    
+    def __str__(self):
+        return self.name
