b4411f20b877446b2d1a64a8f27ee1d4a9c3f56c
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-05-23 13:40:36 +0600

    created Patient  model

--- backend/patient/admin.py
+++ backend/patient/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Patient
+
+
+class PatientModelAdmin(admin.ModelAdmin):
+    list_display = [ 'username', 'first_name','last_name' ,'phone']
+    def username(self, obj):
+        return obj.user.username
+    def first_name(self, obj):
+        return obj.user.first_name
+    def last_name(self, obj):
+        return obj.user.last_name
+
+admin.site.register(Patient, PatientModelAdmin)
--- backend/patient/models.py
+++ backend/patient/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class Patient(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='patient/images/')
+    phone = models.CharField(max_length=11)
+    
+    def __str__(self):
+        return f'{self.user.username}'
