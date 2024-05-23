b22f18b46a6a32ef6c9de363c45cac1daf964211
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-05-23 13:11:53 +0600

    created contact us  model

--- backend/contact_us/admin.py
+++ backend/contact_us/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import ContactUs
+
+
+class ContactUsModelAdmin(admin.ModelAdmin):
+    list_display = ['name', 'phone', 'problem']
+    list_per_page = 10
+
+admin.site.register(ContactUs, ContactUsModelAdmin)
