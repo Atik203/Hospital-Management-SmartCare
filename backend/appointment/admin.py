70af47276b12fb4f26c2b9076d4010bb35bd97bd
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-05-23 15:00:22 +0600

    created appointment model

--- backend/appointment/admin.py
+++ backend/appointment/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Appointment
+
+
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ['patient_name', 'doctor_name', 'type', 'status', 'time', 'cancel']
+    
+    def patient_name(self, obj):
+        return obj.patient.user.first_name + ' ' + obj.patient.user.last_name
+    def doctor_name(self, obj):
+        return obj.doctor.user.first_name + ' ' + obj.doctor.user.last_name
+
+
+admin.site.register(Appointment, AppointmentAdmin)
--- backend/appointment/models.py
+++ backend/appointment/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
+from doctor.models import AvailableTime, Doctor
+from patient.models import Patient
+
 # Create your models here.
+
+TYPE = [
+    ('Online', 'Online'),
+    ('Offline', 'Offline'),
+]
+
+APPOINTMENT_STATUS = [
+    ('Pending', 'Pending'),
+    ('Completed', 'Completed'),
+    ('Running', 'Running'),
+]
+class Appointment(models.Model):
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    type = models.CharField(choices=TYPE, max_length=10)
+    status = models.CharField(choices=APPOINTMENT_STATUS, max_length=10, default='Pending')
+    symptom = models.TextField()
+    time = models.OneToOneField(AvailableTime, on_delete=models.CASCADE)
+    cancel = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return f'{self.patient.user.username} - {self.doctor.user.first_name} {self.doctor.user.last_name} - {self.time.name}'
+    
+    
+    
