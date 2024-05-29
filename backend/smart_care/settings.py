580acf6191a8b4ddcd3706ffd19f8de5c9fbafe0
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-05-29 22:59:09 +0600

    added requirements

--- backend/smart_care/settings.py
+++ backend/smart_care/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-5%e%!6r*adxg#k$fga6^6dgq0i#co_3c2(iu#e!zqb=uz-%1mr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
