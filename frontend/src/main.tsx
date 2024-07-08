526f64a684063aaf8319e819d1bde47799c7464a
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-07-08 15:24:26 +0600

    update redux baseApi and added sonner toaster

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,15 +5,28 @@ import ReactDOM from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
 import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
+import { Toaster } from "sonner";
 import "./index.css";
 import { store } from "./redux/store";
 import router from "./routes/routes";
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <HelmetProvider>
     <NextUIProvider>
       <React.StrictMode>
         <Provider store={store}>
+          <Toaster
+            visibleToasts={10}
+            richColors={true}
+            toastOptions={{
+              style: {
+                background: "mediumseagreen",
+                color: "white",
+                fontSize: "16px",
+                padding: "16px",
+                borderRadius: "6px",
+              },
+            }}
+          />
           <RouterProvider router={router} />
           <SpeedInsights />
         </Provider>
