7729bf646b28e3cade7c13a09ed147cf37b7a211
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-07-08 15:55:54 +0600

    update userSlice

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,31 +5,35 @@ import ReactDOM from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
 import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
 import { Toaster } from "sonner";
 import "./index.css";
-import { store } from "./redux/store";
+import { persistor, store } from "./redux/store";
 import router from "./routes/routes";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <HelmetProvider>
     <NextUIProvider>
       <React.StrictMode>
-        <Provider store={store}>
-          <Toaster
-            visibleToasts={10}
-            richColors={true}
-            toastOptions={{
-              style: {
-                background: "mediumseagreen",
-                color: "white",
-                fontSize: "16px",
-                padding: "16px",
-                borderRadius: "6px",
-              },
-            }}
-          />
-          <RouterProvider router={router} />
-          <SpeedInsights />
-        </Provider>
+        <PersistGate loading={null} persistor={persistor}>
+          <Provider store={store}>
+            <Toaster
+              visibleToasts={10}
+              richColors={true}
+              toastOptions={{
+                style: {
+                  background: "mediumseagreen",
+                  color: "white",
+                  fontSize: "16px",
+                  padding: "16px",
+                  borderRadius: "6px",
+                },
+              }}
+            />
+            <RouterProvider router={router} />
+            <SpeedInsights />
+          </Provider>
+        </PersistGate>
       </React.StrictMode>
     </NextUIProvider>
   </HelmetProvider>
